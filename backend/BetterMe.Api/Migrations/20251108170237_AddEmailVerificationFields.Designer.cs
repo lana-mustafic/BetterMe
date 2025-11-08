4240385161e06d8d4b07d6585e68cb63a1ebd2fc
lana-mustafic <lana.mustafic@edu.fit.ba>
2025-11-08 18:04:26 +0100

    Add email verification DB fields

--- backend/BetterMe.Api/Migrations/20251108170237_AddEmailVerificationFields.Designer.cs
+++ backend/BetterMe.Api/Migrations/20251108170237_AddEmailVerificationFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BetterMe.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BetterMe.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251108170237_AddEmailVerificationFields")]
+    partial class AddEmailVerificationFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/BetterMe.Api/Migrations/20251108170237_AddEmailVerificationFields.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BetterMe.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerificationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailVerificationToken",
+                table: "Users",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmailVerificationTokenExpires",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailVerified",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetToken",
+                table: "Users",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetTokenExpires",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationTokenExpires",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsEmailVerified",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetTokenExpires",
+                table: "Users");
+        }
+    }
+}
