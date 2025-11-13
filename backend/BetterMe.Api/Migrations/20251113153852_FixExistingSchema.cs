d09d3bff33e095d1c294967ceecd35b9df05d02c
lana-mustafic <lana.mustafic@edu.fit.ba>
2025-11-13 16:39:49 +0100

    fix: add empty migration to sync database schema

---
+++ backend/BetterMe.Api/Migrations/20251113153852_FixExistingSchema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BetterMe.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixExistingSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
